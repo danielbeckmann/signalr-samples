d6325c1293f88aafb9641baf1a4da11e037c2286
Daniel Beckmann <daniel.beckmann@acando.de>
2017-12-15 13:53:27 +0100

    Added first sample

---
+++ basic-chat-with-js/ChatHub.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatDemo
+{
+    public class ChatHub : Hub
+    {
+       public Task Send(string message)
+        {
+            return Clients.All.InvokeAsync("broadcast", message);
+        }
+    }
+}
