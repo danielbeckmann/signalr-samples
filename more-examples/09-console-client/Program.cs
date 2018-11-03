9f047590713e3b11d494541754208e11f801450d
Daniel <danielbeckmann@live.de>
2018-11-03 16:45:51 +0100

    Added console client demo

---
+++ more-examples/09-console-client/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace signalrdemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Listen().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task Listen()
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:56428/chat")
+                .Build();
+
+            connection.On<string>("broadcast", (message) =>
+            {
+                Console.WriteLine(message);
+            });
+
+            await connection.StartAsync();
+            Console.WriteLine("Listening...");
+        }
+    }
+}
